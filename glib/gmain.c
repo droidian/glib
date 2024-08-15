baa76d643c79b9e8e1782e86d06507d39c8df874
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-08-15 13:37:22 +0200

    gmain: handle kernels without P_PIDFD support in waitid(2)
    idtype P_PIDFD has only been introduced in Linux 5.4 [0] [1].
    
    [0] https://www.man7.org/linux/man-pages/man2/wait.2.html
    [1] https://github.com/torvalds/linux/commit/3695eae5fee0605f316fbaad0b9e3de791d7dfaf
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- glib/gmain.c
+++ glib/gmain.c
@@ -6135,7 +6135,7 @@ g_child_watch_dispatch (GSource    *source,
                 return TRUE;
               }
           }
-        else
+        else if (errno != EINVAL) /* P_PIDFD support has been introduce in Linux 5.4 */
           {
             int errsv = errno;
 
